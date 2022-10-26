ecdfcd37fed1cce5ac6edc1e8a3016f8ce41b02b
김동진 <dongjin403@naver.com>
2022-10-26 23:37:46 +0900

    유효한 팰린드롬

--- src/chapter1/Main.java
+++ src/chapter1/Main.java
@@ -4,13 +4,23 @@ import java.util.Scanner;
 class Main {
 	public String solution(String str) {
 		String answer = "";
-		return answer;
+		for(int i =0; i<str.length(); i++) {
+			if(Character.isAlphabetic(str.charAt(i))) {
+				answer += str.charAt(i);
+			}
+		}
+		String tmp = new StringBuilder(answer).reverse().toString().toUpperCase();
+		if(tmp.equals(answer.toUpperCase())){
+			return "YES";
+		} else {
+			return "NO";
+		}
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-		String str = kb.next();
+		String str = kb.nextLine();
 		System.out.println(T.solution(str));
 	}
 }
