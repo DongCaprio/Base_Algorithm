c499ea3f08c13d697580f5dc8e9d039ce8999ce8
김동진 <dongjin403@naver.com>
2022-10-28 22:42:10 +0900

    숫자인지 확인 -> Character.isDigit( )

--- src/chapter1/Main.java
+++ src/chapter1/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 class Main {
 	public Integer solution(String str) {
 		String answer = "";
+		/*
 		String change = str.replaceAll("[^0-9]", "");
-
 		int change2 = Integer.parseInt(change);
 		String change3 = Integer.toString(change2);
 		for (int i = 0; i < change3.length(); i++) {
@@ -14,6 +14,11 @@ class Main {
 		}
 		if(answer.length() > 8) {
 			answer.substring(7);
+		}
+		return Integer.parseInt(answer);
+		*/
+		for(char x : str.toCharArray()) {
+			if(Character.isDigit(x)) answer += x;
 		}
 		return Integer.parseInt(answer);
 	}
