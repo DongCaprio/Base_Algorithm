fec615ea66850bfed723e54eb05caa4060841222
김동진 <dongjin403@naver.com>
2022-12-12 23:27:43 +0900

    배열 while로 합치기

--- src/chapter3/Main두배열합치기.java
+++ src/chapter3/Main두배열합치기.java
@@ -1,36 +1,25 @@
 package chapter3;
+
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-class Main {
+class Main두배열합치기 {
 	public List<Integer> solution(int n1, int[] arr1, int n2, int[] arr2) {
 		List<Integer> intList = new ArrayList<>();
-		
-		Arrays.sort(arr1);
-		Arrays.sort(arr2);
-		
-		
-		int a1 = 0, a2 = 0;
-		while(a1 < n1 && a2 < n2) {
-			if(arr1[a1]<arr2[a2]) {
-				a1++;
-			} else if(arr1[a1] == arr2[a2]) {
-				intList.add(arr1[a1]);
-				a1++;
-				a2++;
-			}
-			else {
-				a2++;
-			}
+		for (int i = 0; i < n1; i++) {
+			intList.add(arr1[i]);
 		}
-		
+		for (int i = 0; i < n2; i++) {
+			intList.add(arr2[i]);
+		}
+		Collections.sort(intList);
 		return intList;
 	}
-	
+
 	public static void main(String[] args) {
-		Main T = new Main();
+		Main두배열합치기 T = new Main두배열합치기();
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
 		int[] aa = new int[n];
