14cc2479dbcf6b0b274ad8fee3819b40b5d3df3a
김동진 <dongjin403@naver.com>
2023-01-03 00:12:13 +0900

    스택과 큐

--- src/chapter5/Main.java
+++ src/chapter5/Main.java
@@ -1,12 +1,25 @@
 package chapter5;
+
 import java.util.Scanner;
+import java.util.Stack;
 
 class Main {
 	public String solution(String str) {
-		String answer = "";
+		String answer = "YES";
+		char[] arr = str.toCharArray();
+		Stack<Character> stack = new Stack<Character>();
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == '(') stack.push(arr[i]);
+			if(stack.empty()) {
+				return "NO";
+			}
+			if(arr[i] == ')') stack.pop();
+		}
+		if(!stack.empty()) return "NO";
+
 		return answer;
 	}
-	
+
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
